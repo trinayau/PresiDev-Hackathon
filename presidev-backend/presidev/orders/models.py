f39a10bfaeef17707caca1e64d7af2b24fe1a5db
Paul Martin <paulmartin91@hotmail.co.uk>
2023-01-07 16:58:29 +0000

    Added orders app, models (not migrated).

---
+++ presidev-backend/presidev/orders/models.py
@@ -0,0 +1,69 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=128, null=False, blank=False)
+
+
+class OrganisationType(models.Model):
+    name = models.CharField(max_length=128, null=False, blank=False)
+    description = models.CharField(max_length=256, null=False, blank=False)
+
+
+class UserType(models.Model):
+    name = models.CharField(max_length=128, null=False, blank=False)
+    description = models.CharField(max_length=256, null=False, blank=False)
+
+
+class Location(models.Model):
+    country = models.CharField(max_length=128, null=False, blank=False)
+
+
+class Organisation(models.Model):
+    name = models.CharField(max_length=128, null=False, blank=False)
+    description = models.CharField(max_length=256, null=False, blank=False)
+    verified = models.BooleanField(default=False)
+    organisation_type = models.ForeignKey(OrganisationType)
+    location = models.ForeignKey(Location)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=128, null=False, blank=False)
+
+
+class Order(models.Model):
+    name = models.CharField(max_length=128, null=False, blank=False)
+    description = models.CharField(max_length=256, null=False, blank=False)
+    status = models.ForeignKey(Status)
+    owner = models.ForeignKey(Organisation)
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=128, null=False, blank=False)
+    description = models.CharField(max_length=256, null=False, blank=False)
+    image_url = models.URLField(null=True, blank=True)
+    orders = models.ManyToManyField(Order, through='OrderItems')
+    categories = models.ManyToManyField(Category)
+
+
+class OrderItems(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+
+class ItemPricing(models.Model):
+    quantity = models.IntegerField()
+    cost = models.FloatField()
+    item = models.ForeignKey(Item)
+
+
+"""
+
+    hub id
+    items that are sent need ids
+    each user in the chain needs to be associated with other groups
+    admin profile
+
+"""
--- presidev-backend/presidev/presidev/urls.py
+++ presidev-backend/presidev/presidev/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 from rest_framework_simplejwt.views import (
@@ -23,8 +23,8 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/v1/test/', views.test),
+    path('api/v1/test/', views.test)
 ]
