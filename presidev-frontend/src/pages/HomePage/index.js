99295ec3cee5975691494be718637e9b994603e2
Trina <92634994+trinayau@users.noreply.github.com>
2023-01-07 22:56:40 +0000

    add hero

--- presidev-frontend/src/pages/HomePage/index.js
+++ presidev-frontend/src/pages/HomePage/index.js
@@ -13,6 +13,7 @@ const HomePage = () => {
 
 
     return ( 
+        <>
         <section id="hero" className="hero">
         <div className="container position-relative">
         <div className="row gy-5" data-aos="fade-in">
@@ -35,32 +36,36 @@ const HomePage = () => {
 
           <div class="col-xl-3 col-md-6" data-aos="fade-up" data-aos-delay="100">
             <div class="icon-box">
-              <div class="icon"><i class="bi bi-easel"></i></div>
-              <h4 class="title"><a href="" class="stretched-link">Lorem Ipsum</a></h4>
+              <div class="icon"><i class="bi bi-globe-europe-africa"></i></div>
+              <h4 class="title"><a href="" class="stretched-link">Coordination</a></h4>
+              <p>Provide recommendation and coordination services to international stakeholders</p>
             </div>
           </div>
     
-
           <div class="col-xl-3 col-md-6" data-aos="fade-up" data-aos-delay="200">
             <div class="icon-box">
-              <div class="icon"><i class="bi bi-gem"></i></div>
-              <h4 class="title"><a href="" class="stretched-link">Sed ut perspiciatis</a></h4>
+              <div class="icon"><i class="bi bi-box2-heart"></i></div>
+              <h4 class="title"><a href="" class="stretched-link">Supplies</a></h4>
+              <p>Providing food and vital supplies to communities in crisis in Afghanistan and Ukraine</p>
             </div>
+
           </div>
 
 
           <div class="col-xl-3 col-md-6" data-aos="fade-up" data-aos-delay="300">
             <div class="icon-box">
               <div class="icon"><i class="bi bi-geo-alt"></i></div>
-              <h4 class="title"><a href="" class="stretched-link">Magni Dolores</a></h4>
+              <h4 class="title"><a href="" class="stretched-link">Representation</a></h4>
+              <p>Represent the needs of people affected by violence or poverty to international-policy makers</p>
             </div>
           </div>
  
 
           <div class="col-xl-3 col-md-6" data-aos="fade-up" data-aos-delay="500">
             <div class="icon-box">
-              <div class="icon"><i class="bi bi-command"></i></div>
-              <h4 class="title"><a href="" class="stretched-link">Nemo Enim</a></h4>
+              <div class="icon"><i class="bi bi-airplane-engines"></i></div>
+              <h4 class="title"><a href="" class="stretched-link">Logistical Aid</a></h4>
+              <p>Ensure that humanitarian aid and services reach vulnerable and displaced civilian populations</p>
             </div>
           </div>
 
@@ -70,6 +75,50 @@ const HomePage = () => {
     </div>       
         
         </section>
+        <main>
+        <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-header">
+          <h2>About Us</h2>
+          <p>Presidium Platform is developed and maintained by Team PresiDev.</p>
+        </div>
+
+        <div class="row gy-4">
+          <div class="col-lg-6">
+            <h3>Voluptatem dignissimos provident quasi corporis</h3>
+            <img src="assets/img/about.jpg" class="img-fluid rounded-4 mb-4" alt=""/>
+            <p>Ut fugiat ut sunt quia veniam. Voluptate perferendis perspiciatis quod nisi et. Placeat debitis quia recusandae odit et consequatur voluptatem. Dignissimos pariatur consectetur fugiat voluptas ea.</p>
+            <p>Temporibus nihil enim deserunt sed ea. Provident sit expedita aut cupiditate nihil vitae quo officia vel. Blanditiis eligendi possimus et in cum. Quidem eos ut sint rem veniam qui. Ut ut repellendus nobis tempore doloribus debitis explicabo similique sit. Accusantium sed ut omnis beatae neque deleniti repellendus.</p>
+          </div>
+          <div class="col-lg-6">
+            <div class="content ps-0 ps-lg-5">
+              <p class="fst-italic">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                magna aliqua.
+              </p>
+              <ul>
+                <li><i class="bi bi-check-circle-fill"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+                <li><i class="bi bi-check-circle-fill"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
+                <li><i class="bi bi-check-circle-fill"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
+              </ul>
+              <p>
+                Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident
+              </p>
+
+              <div class="position-relative mt-4">
+                <img src="assets/img/about-2.jpg" class="img-fluid rounded-4" alt=""/>
+                <a href="https://www.youtube.com/watch?v=LXb3EKWsInQ" class="glightbox play-btn"></a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+        </main>
+        </>
 );
 }
  
