0368226e3664e59a4c7d8bdd139956252a7845d5
Trina <92634994+trinayau@users.noreply.github.com>
2023-01-08 00:41:36 +0000

    add login page

--- presidev-frontend/src/App.js
+++ presidev-frontend/src/App.js
@@ -1,12 +1,13 @@
 import {Routes, Route} from 'react-router-dom';
 import {default as Layout} from './layouts';
-import { HomePage } from './pages';
+import { HomePage, LoginPage } from './pages';
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Layout />} >
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
         </Route>
 
       </Routes>
