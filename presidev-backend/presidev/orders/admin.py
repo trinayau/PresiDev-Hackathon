02d1e33be009a4d53b385c24e4ffe85a73a265ad
Paul Martin <paulmartin91@hotmail.co.uk>
2023-01-09 14:04:19 +0000

    Registered models admin.

--- presidev-backend/presidev/orders/admin.py
+++ presidev-backend/presidev/orders/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Organisation, ItemPricing, Location, Order, OrganisationType, User, UserType, Category, Item, Status, OrderItems
 
 # Register your models here.
+admin.site.register(Organisation)
+admin.site.register(ItemPricing)
+admin.site.register(Location)
+admin.site.register(Order)
+admin.site.register(OrganisationType)
+admin.site.register(UserType)
+admin.site.register(Category)
+admin.site.register(Item)
+admin.site.register(Status)
+admin.site.register(OrderItems)
