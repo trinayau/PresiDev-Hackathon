3a8349c475d9c5588312f6c7bb018ae0dcd5db93
Paul Martin <paulmartin91@hotmail.co.uk>
2023-01-10 16:00:55 +0000

    orders tab fetches data on render.

--- presidev-frontend/src/components/OrderDetail/index.js
+++ presidev-frontend/src/components/OrderDetail/index.js
@@ -1,7 +1,36 @@
+import axios from 'axios'
+import { useEffect, useState, useContext } from 'react';
+import { API_ENDPOINT } from '../../settings'
+import AuthContext from '../../context/AuthContext';
+
 const OrderDetail = () => {
-    return ( <div className="order-detail">
-        <h1>Orders</h1>
-    </div> );
+
+  const [orders, setOrders] = useState([])
+  let { authTokens } = useContext(AuthContext);
+
+  useEffect(() => {
+    (async () => {
+      
+      try {
+        console.log(authTokens)
+        const response = await axios.get(`${API_ENDPOINT}/orders/order`, {headers: {Authorization: `Bearer ${authTokens.access}`}})
+        if (response.status == 200) {
+          console.log(response)
+          setOrders(response.data)
+        }
+      } catch(err) {
+        console.log(err)
+      }
+      
+  })()
+  }, [])
+
+  return (
+    <div className="order-detail">
+      <h1>Orders</h1>
+        {orders.map(({name, description, status}) => `${name} - ${description} - ${status.name}`)}
+    </div>
+  );
 }
- 
+
 export default OrderDetail;
---
+++ presidev-frontend/src/settings.js
@@ -0,0 +1,7 @@
+const API_URL = 'http://localhost:8000/api/'
+const API_VERSION = 'v1'
+const API_ENDPOINT = API_URL + API_VERSION
+
+export {
+  API_ENDPOINT
+}
