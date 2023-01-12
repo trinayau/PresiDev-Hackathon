460eee969255b525db3cdd39ab5897d1d9dbe1c8
Paul Martin <paulmartin91@hotmail.co.uk>
2023-01-12 16:03:03 +0000

    Added profile section.

--- presidev-frontend/src/components/OrderDetail/index.js
+++ presidev-frontend/src/components/OrderDetail/index.js
@@ -3,7 +3,7 @@ import { useEffect, useState, useContext } from 'react';
 import { API_ENDPOINT } from '../../settings'
 import AuthContext from '../../context/AuthContext';
 import ClipLoader from "react-spinners/ClipLoader";
-import {OrderPageElem} from '../../components'
+import { OrderPageElem } from '../../components'
 
 const OrderDetail = () => {
 
@@ -16,73 +16,71 @@ const OrderDetail = () => {
     margin: "0 auto",
   };
 
-  
+
 
   useEffect(() => {
     (async () => {
-      
+
       try {
-        const response = await axios.get(`${API_ENDPOINT}/orders/order`, {headers: {Authorization: `Bearer ${authTokens.access}`}})
+        const response = await axios.get(`${API_ENDPOINT}/orders/order`, { headers: { Authorization: `Bearer ${authTokens.access}` } })
         if (response.status === 200) {
           setOrders(response.data)
           setLoading(false)
         }
-      } catch(err) {
+      } catch (err) {
         console.log(err)
       }
-      
-  })()
+
+    })()
   }, [])
 
   return (
     <div className="order-detail">
       <h1>Orders</h1>
-        {loading? <ClipLoader
-          size={40}
-          cssOverride={override}
-          color={"#057465"}
-          loading={loading}
-          speedMultiplier={1.5}
-          aria-label="Loading Spinner"
-          data-testid="loader"
-        /> : orders.map((order)=> {
-          return (
+      {loading ? <ClipLoader
+        size={40}
+        cssOverride={override}
+        color={"#057465"}
+        loading={loading}
+        speedMultiplier={1.5}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      /> : orders.map((order) => {
+        return (
           <>
-          <div class="d-flex my-4 flex-wrap">
-                        <div class="box me-4 my-1 bg-light">
-                            <img className="img-fluid"src="https://www.freepnglogos.com/uploads/box-png/cardboard-box-brown-vector-graphic-pixabay-2.png"
-                                alt="box"/>
-                            <div class="d-flex align-items-center mt-2">
-                                <div class="tag">Orders placed</div>
-                                <div class="ms-auto number">{orders.length}</div>
-                            </div>
-                        </div>
-                        <div class="box me-4 my-1 bg-light">
-                            <img src="https://www.freepnglogos.com/uploads/shopping-cart-png/shopping-cart-campus-recreation-university-nebraska-lincoln-30.png"
-                                alt="cart"/>
-                            <div class="d-flex align-items-center mt-2">
-                                <div class="tag">Items in Cart</div>
-                                <div class="ms-auto number">10</div>
-                            </div>
-                        </div>
-                        <div class="box me-4 my-1 bg-light">
-                            <img src="https://www.freepnglogos.com/uploads/love-png/love-png-heart-symbol-wikipedia-11.png"
-                                alt="heart"/>
-                            <div class="d-flex align-items-center mt-2">
-                                <div class="tag">Favourites</div>
-                                <div class="ms-auto number">10</div>
-                            </div>
-                        </div>
-                        </div>
-                       
+            <div class="d-flex my-4 flex-wrap">
+              <div class="box me-4 my-1 bg-light">
+                <img className="img-fluid" src="https://www.freepnglogos.com/uploads/box-png/cardboard-box-brown-vector-graphic-pixabay-2.png"
+                  alt="box" />
+                <div class="d-flex align-items-center mt-2">
+                  <div class="tag">Orders placed</div>
+                  <div class="ms-auto number">{orders.length}</div>
+                </div>
+              </div>
+              <div class="box me-4 my-1 bg-light">
+                <img src="https://www.freepnglogos.com/uploads/shopping-cart-png/shopping-cart-campus-recreation-university-nebraska-lincoln-30.png"
+                  alt="cart" />
+                <div class="d-flex align-items-center mt-2">
+                  <div class="tag">Items in Cart</div>
+                  <div class="ms-auto number">10</div>
+                </div>
+              </div>
+              <div class="box me-4 my-1 bg-light">
+                <img src="https://www.freepnglogos.com/uploads/love-png/love-png-heart-symbol-wikipedia-11.png"
+                  alt="heart" />
+                <div class="d-flex align-items-center mt-2">
+                  <div class="tag">Favourites</div>
+                  <div class="ms-auto number">10</div>
+                </div>
+              </div>
+            </div>
 
 
-          <OrderPageElem order={order}/>
+
+            <OrderPageElem order={order} />
 
           </>)
-        }) }
-        
-        {/* orders.map(({name, description, status}) => `${name} - ${description} - ${status.name}` */}
+      })}
     </div>
   );
 }
--- presidev-frontend/src/components/ProfileDetail/index.js
+++ presidev-frontend/src/components/ProfileDetail/index.js
@@ -1,23 +1,95 @@
 import { useContext, useEffect } from "react";
-import {AuthContext} from '../../context/AuthContext'
-import { API_ENDPOINT } from '../../settings'
-import axios from 'axios'
+import { AuthContext } from '../../context/AuthContext'
+
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Avatar, Box } from "@mui/material";
+
+
+
 
 const ProfileDetail = () => {
-    let { user, authTokens } = useContext(AuthContext);
-    useEffect(() => {
-        console.log(user)
-        const getProfileDetail = async () => {
-            const response = await axios.get(`${API_ENDPOINT}/orders/user/?user_type=`, {headers: {Authorization: `Bearer ${authTokens.access}`}})
-            let data = await response.json();
-            console.log(data);
-        };
-        getProfileDetail();
-    }, [user]);
+    let { user } = useContext(AuthContext);
 
-    return ( <div className="profile-detail">
-        <h1>Profile</h1>
-    </div> );
+    return (
+        <Card 
+            sx={{ 
+                minWidth: 500,
+                minHeight: 500,
+                marginLeft: 10
+            }}
+            raised
+        >
+            <CardMedia
+                component={() => 
+                    <Box
+                        style={{
+                            minWidth: '100%',
+                            minHeight: 200,
+                            background: "linear-gradient(180deg, white 50%, var(--color-primary) 50%)",
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center'
+                        }}
+                    >
+                        <Avatar 
+                            sx={{width: 125, height: 125, bgcolor: 'var(--color-primary)', border: 1}}
+                        >{user?.name[0]}</Avatar>
+                    </Box>
+                }
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h4">
+                    {user?.name}
+                </Typography>
+                <Typography variant="h5" color="text.secondary">
+                    {user?.profile?.organisation?.name}
+                </Typography>
+                <Typography variant="h6" color="text.secondary">
+                    User Type: {user?.profile?.user_type?.name}
+                </Typography>
+                <br />
+                <br />
+                <Typography gutterBottom variant="h5" sx={{textDecoration: 'underline'}}>
+                    Location
+                </Typography>
+                {user?.profile?.location?.street_number && 
+                    <Typography variant="h5" color="text.secondary">
+                        {user?.profile?.location?.street_number},
+                    </Typography>
+                }
+                <Typography variant="h5" color="text.secondary">
+                    {user?.profile?.location?.first_line},
+                </Typography>
+                {user?.profile?.location?.second_line && 
+                    <Typography variant="h5" color="text.secondary">
+                        {user?.profile?.location?.second_line},
+                    </Typography>
+                }
+                <Typography variant="h5" color="text.secondary">
+                    {user?.profile?.location?.county},
+                </Typography>
+                <Typography variant="h5" color="text.secondary">
+                    {user?.profile?.location?.country},
+                </Typography>
+                <Typography variant="h5" color="text.secondary">
+                    {user?.profile?.location?.postcode}
+                </Typography>
+                <br />
+                <br />
+                <Typography gutterBottom variant="h5" color="text.secondary">
+                    Phone: {user?.profile?.phone}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Update Details</Button>
+            </CardActions>
+        </Card>
+    );
 }
- 
+
 export default ProfileDetail;
