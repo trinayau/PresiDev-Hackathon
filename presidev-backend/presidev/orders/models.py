d6bf55b33fccf1da411bbc1daa5cdf29a280c13b
Paul Martin <paulmartin91@hotmail.co.uk>
2023-01-15 11:06:16 +0000

    Added url to Item model.

--- presidev-backend/presidev/orders/models.py
+++ presidev-backend/presidev/orders/models.py
@@ -49,6 +49,7 @@ class Item(models.Model):
     image_url = models.URLField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     categories = models.ManyToManyField(Category)
+    url = models.CharField(max_length=256, null=True)
 
     def __str__(self):
        return self.name
