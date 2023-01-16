67810d81f3ee202a69134cce441aac7f8c217e7b
Paul Martin <paulmartin91@hotmail.co.uk>
2023-01-16 16:05:15 +0000

    Added highlighted page selection on nav.

--- presidev-frontend/src/components/Sidebar/index.js
+++ presidev-frontend/src/components/Sidebar/index.js
@@ -1,42 +1,45 @@
 import { Box, List, ListItemButton, ListItem, ListItemText } from "@mui/material";
 import { Link } from "react-router-dom";
 import AuthContext from '../../context/AuthContext';
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 
+const nav = {
+  'End User': ['account', 'orders', 'favourites', 'categories', 'products', 'cart'],
+  'Operational Hub': ['account', 'orders'],
+  'Supplier': ['account', 'orders']
+}
 
 const Sidebar = () => {
-    let { user } = useContext(AuthContext);
-    
-    const userType = user.profile.organisation.organisation_type.name
-    
-    const nav = {
-        'End User': ['account', 'orders', 'favourites', 'categories', 'products', 'cart'],
-        'Operational Hub': ['account', 'orders'],
-        'Supplier': ['account', 'orders']
-    }
+  let { user } = useContext(AuthContext);
+  const [active, setActive] = useState('account')
+  const userType = user.profile.organisation.organisation_type.name
+  
 
-    return (
+  return (
 
-        <Box
-            sx={{ marginTop: 10 }}
-        >
-            <List>
-                {nav[userType].map(page =>
-                    <ListItem>
-                        <ListItemButton
-                            to={`/${page}`}
-                            component={Link}
-                        >
-                            <ListItemText primary={page.toUpperCase()} />
-                        </ListItemButton>
-                    </ListItem>
-                )}
-            </List>
+    <Box
+      sx={{ marginTop: 10 }}
+    >
+      <List>
+        {nav[userType].map(page =>
+          <ListItem
+            active={true}
+          >
+            <ListItemButton
+              to={`/${page}`}
+              component={Link}
+              onClick={() => setActive(page)}
+            >
+              <ListItemText primary={page.toUpperCase()} sx={{ color: page === active ? 'var(--color-primary)' : '' }} />
+            </ListItemButton>
+          </ListItem>
+        )}
+      </List>
 
-        </Box>
+    </Box>
 
-    )
+  )
 }
 
 export default Sidebar;
