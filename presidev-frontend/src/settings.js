ce1b9bc721aec79c1b07c8f4fb19850c662f8108
Paul Martin <paulmartin91@hotmail.co.uk>
2023-01-18 11:08:11 +0000

    Changed endpoints.

--- presidev-frontend/src/settings.js
+++ presidev-frontend/src/settings.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:8000/api/'
+const API_URL = 'https://presidev.onrender.com/api/'// 'http://localhost:8000/api/'
 const API_VERSION = 'v1'
 const API_ENDPOINT = API_URL + API_VERSION
 
